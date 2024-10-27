8294021f089cce6522a2105b893eed6c6336c5b8
Slavomir Sidor <slavomir.sidor@gmail.com>
2024-10-27 17:50:27 +0100

    Examples implementations.

--- examples/instance/delete.js
+++ examples/instance/delete.js
@@ -6,32 +6,42 @@
  * @copyright 2024 Elrest AutomationsSysteme GMBH
  */
 
+
 const WDXWSClient = require('@wago/wdx-ws-client-js');
+const WDXWSClientConfiguration = require('../configuration/configuration.js');
 
 (async () => {
     try {
-        const c = new WDXWSClient.ClientService({ protocol: 'ws', host: 'localhost', port: 4282 });
+        const c = new WDXWSClient.WDX.WS.Client.JS.Service.ClientService(
+            WDXWSClientConfiguration.wsConfiguration
+        );
+        console.log('Connecting');
         await c.connect();
-
         console.log('Connected successfully');
 
-        c.instanceService.delete('1d64e8c4-53d7-11ef-b262-088fc37eff34').subscribe(
+        c.instanceService.delete('b8ed912d-db48-4165-937c-6e7e039fedc9').subscribe(
             {
-                next: (instance) => {
-                    console.log(instance);
+                next: (response) => {
+                    console.log('Response');
+                    console.log(JSON.stringify(response, null, 2));
                 },
 
                 error: async (error) => {
-                    console.error('Error: ' + error.message);
+                    console.error('Error Code: ' + error.code);
+                    console.error('Error Message: ' + error.message);
+                    console.error('Error Stack: ' + error.stack);
 
+                    console.log('Disconnecting');
                     await c.disconnect();
                     console.log('Disconnected successfully');
                 },
 
                 complete: async () => {
+                    console.log('Completed');
+                    console.log('Disconnecting');
                     await c.disconnect();
                     console.log('Disconnected successfully');
-                }
+                },
             },
         );
 
--- examples/instance/restart.js
+++ examples/instance/restart.js
@@ -7,28 +7,37 @@
  */
 
 const WDXWSClient = require('@wago/wdx-ws-client-js');
+const WDXWSClientConfiguration = require('../configuration/configuration.js');
 
 (async () => {
     try {
-        const c = new WDXWSClient.ClientService({ protocol: 'ws', host: 'localhost', port: 4282 });
+        const c = new WDXWSClient.WDX.WS.Client.JS.Service.ClientService(
+            WDXWSClientConfiguration.wsConfiguration
+        );
+        console.log('Connecting');
         await c.connect();
-
         console.log('Connected successfully');
 
-        c.instanceService.restart('1d64e8c4-53d7-11ef-b262-088fc37eff34').subscribe(
+
+        c.instanceService.restart('b8ed912d-db48-4165-937c-6e7e039fedc9').subscribe(
             {
-                next: (instance) => {
-                    console.log(instance);
+                next: (response) => {
+                    console.log('Response');
+                    console.log(JSON.stringify(response, null, 2));
                 },
 
                 error: async (error) => {
-                    console.error('Error: ' + error.message);
+                    console.error('Error Code: ' + error.code);
+                    console.error('Error Message: ' + error.message);
 
+                    console.log('Disconnecting');
                     await c.disconnect();
                     console.log('Disconnected successfully');
                 },
 
                 complete: async () => {
+                    console.log('Completed');
+                    console.log('Disconnecting');
                     await c.disconnect();
                     console.log('Disconnected successfully');
                 }
